e436ab2b61dcfdb26855e62f89af6fcba93f0908
mydevby <mydev.by@gmail.com>
2021-09-12 00:02:16 +0300

    Hello my friend

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,45 @@
 <script>
-	let name = 'Valha';
-	let color = 'black';
+	let firstName = 'Nick';
+	let lastName = 'Freeman';
+	let color = '';
 	let size = 18;
 
 	const colorText = (e) => {
 		color = e.target.value;
 	};
+
+	const firstNameText = (e) => {
+		firstName = e.target.value;
+	};
+
+	const lastNameText = (e) => {
+		lastName = e.target.value;
+	};
 </script>
 
 <main>
-	<h1 style="color: {color}; font-size: {size}px;">Hello {name}!</h1>
+	<h1 style="color: {color}; font-size: {size}px;">Hello {firstName} {lastName}!</h1>
 	<form>
-		<label>
-			Color text:
-			<input type="color" on:input={colorText} value={color}>
-		</label>
-		<label>
-			Size text:
-			<input type="range" bind:value={size} min="12" max="30">
-		</label>
+		<fieldset>
+			<legend>Personal Data:</legend>
+			<label>First name: 
+				<input type="text" on:input={firstNameText} value={firstName}>
+			</label>
+			<label>Last name: 
+				<input type="text" on:input={lastNameText} value={lastName}>
+			</label>
+		</fieldset>
+		<fieldset>
+			<legend>Style:</legend>
+			<label>
+				Color text:
+				<input type="color" on:input={colorText} value={color}>
+			</label>
+			<label>
+				Size text:
+				<input type="range" bind:value={size} min="12" max="30">
+			</label>
+		</fieldset>
 	</form>
 </main>
 
